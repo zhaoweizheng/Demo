03df475e3350c9212b22c2a79c85551b1d9f51af
zhaoweizheng <zhaoweizheng66@163.com>
2015-06-08 15:14:46 +0800

    隐藏navigationBar

---
+++ MovingNavigationBar/KOMovingNavigationBarExample/KOViewController.m
@@ -0,0 +1,44 @@
+//
+//  KOViewController.m
+//  KOMovingNavigationBar
+//
+//  Created by Oleg Kohtenko on 07.06.13.
+//  Copyright (c) 2013 Kohtenko. All rights reserved.
+//
+
+#import "KOViewController.h"
+
+@interface KOViewController ()
+
+@end
+
+@implementation KOViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    self.navigationItem.title = @"Code4App";
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark
+#pragma UITableView delegate & dataSource methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{ return 100; }
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if(!cell) cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    cell.textLabel.text = [NSString stringWithFormat:@"Cell %d", indexPath.row];
+    return cell;
+}
+
+
+
+@end
---
+++ MovingNavigationBar/README.md
@@ -0,0 +1,9 @@
+MovingNavigationBar
+===================
+Moving UINavigationBar with UIScrollView or table for iOS
+
+![Output sample](https://raw.github.com/kohtenko/MovingNavigationBar/master/animationExample.gif)
+
+
+All you need to do is make you Controller inherited from KOMovingBarViewController class and fill up IBOutlet scrollForHideNavigation. For better look your navigation Bar should be Translucent 
+`self.navigationController.navigationBar.barStyle = UIBarStyleBlackTranslucent;`
